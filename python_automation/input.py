76dc43148fbb3c69a1d1aa83108059fce975f122
saurabh2k26 <saurabh2k26@gmail.com>
2018-12-17 19:41:10 +0530

    Create input.py

---
+++ python_automation/input.py
@@ -0,0 +1,6 @@
+print "What is your age"
+age = raw_input()
+print "So, you are %s years old." %(age)
+
+days = raw_input("What was the duration of your last holidays in days?")
+print "%s days!!! Nice!!!" %(days)
