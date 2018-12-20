32bc2dadea6a4707de47b40298ca2bf9be9a760f
saurabh2k26 <saurabh2k26@gmail.com>
2018-12-20 20:05:06 +0530

    Create while_loop.py

---
+++ python_automation/while_loop.py
@@ -0,0 +1,10 @@
+i = 1
+while(i < 4):
+  print i
+  i = i + 1
+  
+#program to take user input until user type- "end"
+input = ""
+while(input != "end"):
+  print "Please enter input"
+  input = raw_input()
