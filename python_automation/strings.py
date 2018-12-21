b649bd7c6b946c8ecd57175a7e7327ec8bb72a31
saurabh2k26 <saurabh2k26@gmail.com>
2018-12-21 16:41:25 +0530

    Create strings.py

---
+++ python_automation/strings.py
@@ -0,0 +1,19 @@
+a1 = "I love Bengaluru"
+a2 = "testing"
+
+print a2[0:4]    #index 0 to index 3
+
+print a1.split(" ")[2]    #spliting to list by seperator 'space' and printing 2nd index
+
+print a2[::-1]    #reversing a string
+
+print 'love' in a1    #searching for a word in string
+print 'hate' in a1
+
+print a2.capitalize()   #capitalize first letter
+
+print a2.islower()    #True if all characters are in lowercase
+
+print a2.isdigit()    #True if all characters are digits
+
+print len(a2)    #gives length of string
