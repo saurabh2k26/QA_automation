3c1f6585c996523c0b4849409fa723ad9a428b04
saurabh2k26 <saurabh2k26@gmail.com>
2018-12-21 16:25:50 +0530

    Create numbers.py

---
+++ python_automation/numbers.py
@@ -0,0 +1,19 @@
+a1 = 200
+a2 = 500.21
+print a1, type(a1)
+print a2, type(a2)
+
+a3 = int(a2)  # this is called type conversion
+print a3, type(a3)
+
+a4 = float(a1)
+print a4, type(a4)
+
+a5 = a1/10
+print a5, type(a5)
+
+a6 = a2/10
+print a6, type(a6)
+
+a7 = a2//10     # this is floor division
+print a7, type(a7)
